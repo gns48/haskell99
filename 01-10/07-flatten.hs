bd614306e4fb9548450f484da0c6be7f57d7a4e3
Gleb Semenov <gleb.semenov@gmail.com>
2018-10-03 20:15:13 +0300

    task #7 done

---
+++ 01-10/07-flatten.hs
@@ -0,0 +1,31 @@
+-- Flatten a nested list
+-- (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]]) -> [1,2,3,4,5]
+--
+
+data NestedList a = Elem a | List [NestedList a]
+
+flatten0 :: NestedList a -> [a]
+flatten0 (Elem x) = [x]
+flatten0 (List x) = concatMap flatten0 x
+
+flatten1 :: NestedList a -> [a]
+flatten1 (Elem x) = [x]
+flatten1 (List (x::xs)) = flatten1 x ++ flatten1 (List xs)
+flatten1 (List []) = []
+
+flatten2 :: NestedList a -> [a]
+flatten2 (Elem x) = return x
+flatten2 (List x) = flatten2 =<< x
+                                               
+
+
+
+
+
+                        
+
+                    
+                    
+                    
+
+                  
